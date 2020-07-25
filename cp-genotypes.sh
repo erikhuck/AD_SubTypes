bceae61914b6f80cc488ac23f63090559614baa1
Erik Huckvale <erikhuck@gmail.com>
2020-07-25 13:44:40 -0600

    Made a script for copying the genotypes data into the Cluster repo

---
+++ cp-genotypes.sh
@@ -0,0 +1,5 @@
+# Transfers the combined genotypes data that has been filtered by columns that intersect between the two cohorts
+# TODO: We may want to bring all of that external code and data into this repo for reproducibility before publishing
+
+# ADNI data
+cp ../../AddNeuroMed/Data/Genotypes/tables/ADNI_filtered.csv AD_SubTypesCluster/prepared-data/adni/genotypes.csv
