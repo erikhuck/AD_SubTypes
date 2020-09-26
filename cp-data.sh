56142716dc3053f2992a215ebee77910f962b18f
Erik Huckvale <erikhuck@gmail.com>
2020-09-26 14:55:12 -0600

    Had to change permissions; Updated submodules

--- cp-data.sh
+++ cp-data.sh
@@ -1,18 +1,18 @@
 # Transfers prepared data in the DataPrep repo to the DataClean repo
 
 # ADNI data
-cp AD_SubTypesDataClean/processed-data/filtered-datasets/adni/phenotypes.csv AD_SubTypesCluster/prepared-data/adni/phenotypes.csv
-cp AD_SubTypesDataClean/processed-data/filtered-datasets/adni/phenotypes-col-types.csv AD_SubTypesCluster/prepared-data/adni/phenotypes-col-types.csv
+cp AD_SubTypesDataClean/processed-data/unfiltered-datasets/adni/phenotypes.csv AD_SubTypesCluster/prepared-data/adni/phenotypes.csv
+cp AD_SubTypesDataClean/processed-data/unfiltered-datasets/adni/phenotypes-col-types.csv AD_SubTypesCluster/prepared-data/adni/phenotypes-col-types.csv
 
 echo "Copied phenotypic data"
 
-cp AD_SubTypesDataClean/processed-data/filtered-datasets/adni/expression.csv AD_SubTypesCluster/prepared-data/adni/expression.csv
-cp AD_SubTypesDataClean/processed-data/filtered-datasets/adni/expression-col-types.csv AD_SubTypesCluster/prepared-data/adni/expression-col-types.csv
+cp AD_SubTypesDataClean/processed-data/unfiltered-datasets/adni/expression.csv AD_SubTypesCluster/prepared-data/adni/expression.csv
+cp AD_SubTypesDataClean/processed-data/unfiltered-datasets/adni/expression-col-types.csv AD_SubTypesCluster/prepared-data/adni/expression-col-types.csv
 
 echo "Copied expression data"
 
-cp AD_SubTypesDataClean/processed-data/filtered-datasets/adni/mri.csv AD_SubTypesCluster/prepared-data/adni/mri.csv
-cp AD_SubTypesDataClean/processed-data/filtered-datasets/adni/mri-col-types.csv AD_SubTypesCluster/prepared-data/adni/mri-col-types.csv
+cp AD_SubTypesDataClean/processed-data/unfiltered-datasets/adni/mri.csv AD_SubTypesCluster/prepared-data/adni/mri.csv
+cp AD_SubTypesDataClean/processed-data/unfiltered-datasets/adni/mri-col-types.csv AD_SubTypesCluster/prepared-data/adni/mri-col-types.csv
 
 echo "Copied MRI data"
 
